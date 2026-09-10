ca85ca4c26a7c4a50cb9047106954f9370093d69
Aritra Gorai <thatsaritra@gmail.com>
2026-09-10 14:15:17 +0530

    Fix Vercel deploy: remove incorrect base path

--- Smart_Automation_Frontend/vite.config.js
+++ Smart_Automation_Frontend/vite.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Smart_Automation_Frontend/',
   // server:{
   //   port: 3000,
   // }
