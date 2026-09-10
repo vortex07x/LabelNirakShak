e2de1fa13b2799a72d5a52127749e9a01c6fce0e
Aritra Gorai <thatsaritra@gmail.com>
2026-09-10 14:25:41 +0530

    Fix: remove incorrect basename breaking router on Vercel root deploy

--- Smart_Automation_Frontend/src/main.jsx
+++ Smart_Automation_Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Smart_Automation_Frontend/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
